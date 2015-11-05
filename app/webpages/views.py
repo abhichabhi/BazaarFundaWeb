4249c0cc84cd9049b0f3e65b59196be909a4db86
StratDecider <dude.abhi.chat@gmail.com>
2015-11-05 18:26:13 +0530

    session management

--- app/webpages/views.py
+++ app/webpages/views.py
@@ -146,6 +146,11 @@ def browse(category):
 			_sid = None
 
 		if _sid is not None:
+			try:
+				userDbProducts = mongoUserVariables['listing_products'].find_one({"_sid":_sid})["product_list"]
+			except:
+				mongoUserVariables['listing_products'].insert({"_sid":_sid, "product_list":scoreSortedProductList, "date": utc_timestamp})
+
 			mongoUserVariables['listing_products'].update_one({"_sid":_sid}, { "$set":{ "product_list":scoreSortedProductList, "date": utc_timestamp}})
 		else:
 			uid = uuid.uuid4()
